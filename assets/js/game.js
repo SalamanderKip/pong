c429c1d29b482a1fc4119e0adcf3262eef33bfa0
Maarten <75844243+SalamanderKipp@users.noreply.github.com>
2021-04-06 13:16:57 +0200

    stap 4

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,37 +1,47 @@
-class Game {
+class Game
+{
     constructor() {
         // Verbinden met de ponggame-canvas (moet iedere frame opnieuw getekend worden)
-        this.canvas = document.getElementById('pong');
-        this.context = this.canvas.getContext('2d');
+        this.canvas  = document.getElementById('pong');
+        this.context = this.canvas.getContext('2d');  
 
         // Bal-object aanmaken
-        this.ball = new Ball(this.canvas.width / 2, this.canvas.height / 2, 'orange');
+        this.ball = new Ball(this.canvas.width/2, this.canvas.height/2, 'orange');
 
         // Gameloop
         let lastTime;
         const callback = (milliseconds) => {
-            if (lastTime) {
-                this.update((milliseconds - lastTime) / 1000);
+            if(lastTime) {
+                this.update( (milliseconds - lastTime) / 1000 );
                 this.draw();
             }
             lastTime = milliseconds;
             window.requestAnimFrame(callback);
         }
         callback();
-
-        console.log(this.ball.size.y);
     }
 
     update(deltatime) {
+        this.ball.position.x += this.ball.velocity.x * deltatime; 
+        this.ball.position.y += this.ball.velocity.y * deltatime;
+
+        if(this.ball.bottom > this.canvas.height || this.ball.top < 0) {
+            this.ball.velocity.y = -this.ball.velocity.y; 
+        }
+
+        if(this.ball.right > this.canvas.width || this.ball.left < 0) {
+            this.ball.velocity.x = -this.ball.velocity.x; 
+        }
 
     }
 
     draw() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         // Tekenen van de bal
         this.drawRectangle(this.context, this.ball);
     }
 
-    drawRectangle(ctx, rect, color = 'white') {
+    drawRectangle(ctx, rect, color='white') {
         ctx.fillStyle = color;
         ctx.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
     }
