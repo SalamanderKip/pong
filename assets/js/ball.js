46dbdb3ec3cef2f6007788948a2407da6213a6b7
Maarten <75844243+SalamanderKipp@users.noreply.github.com>
2021-04-20 16:35:07 +0200

    stap 14

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,29 +1,43 @@
-class Ball extends Rectangle
-{
-    constructor(x=0, y=0, color) {
+class Ball extends Rectangle {
+    constructor(x = 0, y = 0, color) {
         super(x, y, 24, 24, color);
-        this.speed  = 0;
+        this.speed = 0;
         this.startX = x;
         this.startY = y;
-        this.angle  = 0;
+        this.angle = 0;
         this.locked = false;
-        this.out    = false;
-        this.velocity.x = (getRandomNumBetween(0,1)===1) ? getRandomNumBetween(-50,-150) : getRandomNumBetween(50,150);   
-        this.velocity.y = (getRandomNumBetween(0,1)===1) ? getRandomNumBetween(-50,-150) : getRandomNumBetween(50,150);
+        this.out = false;
         this.reset();
     }
 
-    reset(x=this.startX, y=this.startY) {
+    reset(x = this.startX, y = this.startY) {
         this.setPosition(x, y);
-        // voeg hier code aan toe die de snelheid van de ball ophoogt
-
+        // voeg hier code aan toe die de snelheid van de ball instelt
+        this.setSpeed(150);
         // voeg hier code aan toe die de hoek van de bal random gaat maken
-
+        this.setAngle(getRandomNumBetween(1, 2) === 1 ? getRandomNumBetween(150, 210) : getRandomNumBetween(330, 390));
         this.out = false;
     }
 
     setPosition(x, y) {
-        this.position.x = x;  
+        this.position.x = x;
         this.position.y = y;
     }
+
+    setSpeed(speed) {
+        this.speed = Math.ceil(speed);
+        console.log('setSpeed(' + this.speed + ')');
+        console.log('velocity = ' + this.velocity.x, this.velocity.y);
+
+    }
+
+    setAngle(degrees) {
+        const radians = this.toRadians(degrees);
+        this.velocity.x = this.speed * Math.cos(radians);
+        this.velocity.y = this.speed * -Math.sin(radians);
+    }
+
+    toRadians(degrees) { return degrees * (Math.PI / 180) }
+    toDegrees(radians) { return (radians * 180) / Math.PI }
+
 }
